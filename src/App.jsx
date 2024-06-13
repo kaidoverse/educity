87f345270dccf7278716f9dd791646db61ceeaa0
kaidoverse <kaidoverse@gmail.com>
2024-06-13 07:12:32 -0400

    mounted hero to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navigation from './Components/Navigation/Navigation'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Hero />
     </div>
   )
 }
