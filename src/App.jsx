62276bb5a94c9be8036de8fe580383746385db73
kaidoverse <kaidoverse@gmail.com>
2024-06-13 07:40:52 -0400

    added programs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './Components/Navigation/Navigation'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navigation />
       <Hero />
+      <Programs />
     </div>
   )
 }
