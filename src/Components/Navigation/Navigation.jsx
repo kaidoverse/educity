8f05e8fb0941c335a968514f5ef853f73fc94e6c
kaidoverse <kaidoverse@gmail.com>
2024-06-18 08:37:32 -0400

    added react scroll, updated testimonials comp and clickable nav links

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navigation.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
 
 const Navigation = () => {
 
@@ -16,12 +17,12 @@ const Navigation = () => {
         <nav className={`container ${sticky ? 'dark-navigation' : ''}`}>
             <img src={logo} alt="" className='logo' />
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contact us</button></li>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
         </nav>
     )
