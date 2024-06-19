1c24b1ab480874307aee53eb6fc14c6a52ff63f7
kaidoverse <kaidoverse@gmail.com>
2024-06-19 06:31:38 -0400

    added media queries and hide menu function for nav component

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navigation.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
 
 const Navigation = () => {
 
@@ -13,10 +14,15 @@ const Navigation = () => {
         ])
     }, []);
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
     return (
         <nav className={`container ${sticky ? 'dark-navigation' : ''}`}>
             <img src={logo} alt="" className='logo' />
-            <ul>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
@@ -24,6 +30,7 @@ const Navigation = () => {
                 <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
                 <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
+            <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
         </nav>
     )
 }
